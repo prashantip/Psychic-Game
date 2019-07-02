957a90ee43c01d46fd5b310198c325b43f1915f5
prashanti <iawwfte@gmail.com>
2019-07-01 23:33:00 -0400

    first update - psuedocode

---
+++ Assets/Javascript/game.js
@@ -0,0 +1,16 @@
+// press any key to start the game
+//once the player press a letter (G)
+//display the players Key (A)
+//if the key gets tallyed 
+//alert  you won
+//score goes up by 1 (player)
+//if the key dosen't get tallyed
+//alret you lost
+//score goes up by a (-1)
+// the payer gets 9 chances
+//computer needs to track letters keyed
+//the game will start over  after 9 keys
+
+
+
+var letters = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,x,y,z];
