053044881e0f9386b50a9feb54a696adb1b6a96e
prashanti <iawwfte@gmail.com>
2019-07-11 21:41:11 -0400

    Horyay My homework

--- Assets/Javascript/game.js
+++ Assets/Javascript/game.js
@@ -1,16 +1,104 @@
-// press any key to start the game
-//once the player press a letter (G)
-//display the players Key (A)
-//if the key gets tallyed 
-//alert  you won
-//score goes up by 1 (player)
-//if the key dosen't get tallyed
-//alret you lost
-//score goes up by a (-1)
-// the payer gets 9 chances
-//computer needs to track letters keyed
-//the game will start over  after 9 keys
+var lettersGuessed = [];
+var wins = 0;
+var losses = 0;
+var computerGuess = ["a", "x", "c", "d", "p", "k", "r"];
+var guessesLeft = 9;
+    
 
 
+// players's keyboard input and make the input lowercase
 
-var letters = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,x,y,z];
+document.onkeyup = function(event) {
+    var keyPress = (String.fromCharCode(event.keyCode)).toLowerCase();
+    addLetter(keyPress);
+
+    document.getElementById("numWins").innerHTML = wins;
+
+    document.getElementById("numGuesses").innerHTML = guessesLeft;
+
+    document.getElementById("numLosses").innerHTML = losses;
+
+}
+//Repeat function and Alert
+function addLetter (usersKeypress) {
+
+    var repeatGuess = lettersGuessed.some(function(item){
+        return item === usersKeypress;
+    })
+
+      if (repeatGuess) {
+        alert(usersKeypress + " already guessed. Try again!");
+        } 
+        else {
+        lettersGuessed.push(usersKeypress);
+
+        console.log(lettersGuessed);
+        showLettersGuessed();
+        guessMatch(usersKeypress);
+    }
+
+}
+
+//function to show letters guessed in browser
+function showLettersGuessed() {
+    var show = lettersGuessed.join(", ");
+    document.getElementById("playerGuess").innerHTML = show;
+}
+
+function guessMatch (character) {
+
+    console.log(character);
+    console.log(computerGuess);
+    var i;
+    var isMatched = false;
+    
+    for (i = 0; i < computerGuess.length; i++) { 
+        if (character === computerGuess[i])
+        {
+            isMatched = true;
+        }
+    }
+    
+    if (isMatched === true) {
+    
+        alert("You win!");
+        wins ++;
+        guessesLeft--;
+        
+        console.log (wins);
+    
+    } 
+   else if(isMatched == false)
+    {
+        guessesLeft--;
+        losses++;
+        
+
+    }
+    else if (guessesLeft === 0) {
+        alert("Start the game over agin");
+        resetVariables ();
+    
+    } 
+}
+
+   
+
+function resetVariables () {
+    lettersGuessed = [];
+    guessesLeft = 9;
+    document.getElementById("numGuesses").innerHTML = 9;
+    document.getElementById("numWins").innerHTML = 0;
+    document.getElementById("numLosses").innerHTML =0;
+    document.getElementById("playerGuess").innerHTML ="";
+    wins=0;
+    losses=0;
+}
+// 
+// // function startGame() {
+//     showGuessesRemaining();
+//     showWins();
+//     showLosses();
+// }
+
+
